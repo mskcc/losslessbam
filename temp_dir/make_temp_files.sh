a118f3a0588936a9fabbdff52ac665253d2e598a
svural <suleymanvural@gmail.com>
2021-01-22 15:50:54 -0500

    updates

--- temp_dir/make_temp_files.sh
+++
@@ -1,4 +0,0 @@
-head -400 DU874145-N_IGO_00000_TEST_L001_R1_001.fastq | gzip > N_L001_R1.fastq.gz
-tail -n400 DU874145-N_IGO_00000_TEST_L001_R1_001.fastq | gzip  > N_L002_R1.fastq.gz
-head -400 DU874145-N_IGO_00000_TEST_L001_R2_001.fastq | gzip > N_L001_R2.fastq.gz
-tail -n400 DU874145-N_IGO_00000_TEST_L001_R2_001.fastq | gzip > N_L002_R2.fastq.gz
